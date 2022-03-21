1b47b2c52b2a88ede1e72370a5ddc7a374504544
Matt Johnson <mrjoh3@gmail.com>
2022-03-21 22:43:21 +1100

    basic working function

--- R/create_rmd.R
+++ R/create_rmd.R
@@ -1,31 +1,35 @@
 
 
-create_rmd <- function(title, metadata, .fun, ...) {
+create_rmd <- function(title, metadata, .fun = NULL, ...) {
 
   # build filename ??  distill already does this
+  slug <- gsub('[[:punct:]]', '', title)
+  slug <- gsub(' |  ', '-', slug)
+  slug <- tolower(slug)
 
+  filename <- paste0(format(Sys.Date(), '%Y-%m-%d'),
+                     '-',
+                     slug,
+                     '.Rmd')
 
   # get all files in wd
   old_files <- list.files(pattern = '.rmd', recursive = TRUE, ignore.case = TRUE)
 
   if (is.null(.fun)) {
-    rmarkdown::draft(...)
+    rmarkdown::draft(filename, edit = FALSE, ...)
   } else {
-    call(.fun, ...)
+    do.call(.fun, args = list(title = title, ...))
   }
 
   # wait for render.
 
-
-
-
   # get filename
-  if ('# not defined') {
+  if (file.exists(filename)) {
+    new_file <- filename
+  } else {
     # detect new file
     new_file <- setdiff(list.files(pattern = '.rmd', recursive = TRUE, ignore.case = TRUE),
                         old_files)
-  } else {
-    new_file <- filename
   }
 
 
@@ -36,6 +40,7 @@ create_rmd <- function(title, metadata, .fun, ...) {
   md <- rmarkdown::yaml_front_matter(new_file)
 
   md$uuid <- uuid::UUIDgenerate()
+  md$draft <- TRUE # renders as yes (yaml spec 1.1) not sure if will cause issues
 
   writeLines(c(paste0("---\n",
                       yaml::as.yaml(md),
@@ -44,5 +49,9 @@ create_rmd <- function(title, metadata, .fun, ...) {
                doc[(yaml_delimiters[2] + 1):length(doc)]),
              new_file)
 
+  if (rstudioapi::hasFun("navigateToFile"))
+    rstudioapi::navigateToFile(new_file)
+  else utils::file.edit(new_file)
 
+  invisible(new_file)
 }
