b179fb64a250db38108076b47140fda7580497f5
Matt Johnson <mrjoh3@gmail.com>
2022-04-04 21:29:19 +1000

    add metadata from template, and arbitrary

--- R/create_rmd.R
+++ R/create_rmd.R
@@ -1,6 +1,16 @@
 
 
-create_rmd <- function(title, metadata, .fun = NULL, ...) {
+#' @title Create a New RMarkdown File
+#'
+#' @param title
+#' @param .fun
+#' @param ... arbitrary metadata added to yaml header
+#'
+#' @return
+#' @export
+#'
+#' @examples
+create_rmd <- function(title, .fun = NULL, ...) {
 
   # build filename ??  distill already does this
   slug <- gsub('[[:punct:]]', '', title)
@@ -39,8 +49,20 @@ create_rmd <- function(title, metadata, .fun = NULL, ...) {
   yaml_delimiters <- grep("^(---|\\.\\.\\.)\\s*$", doc)
   md <- rmarkdown::yaml_front_matter(new_file)
 
+
+
+  # add default entries
   md$uuid <- uuid::UUIDgenerate()
   md$draft <- TRUE # renders as yes (yaml spec 1.1) not sure if will cause issues
+
+  # search for about.yml file in working directory
+  if (file.exists('about.yml')) {
+    template <- yaml::read_yaml('about.yml')
+    md <- c(md, template)
+  }
+
+  # add arbitrary metadata here
+  md <- c(md, ...)
 
   writeLines(c(paste0("---\n",
                       yaml::as.yaml(md),
